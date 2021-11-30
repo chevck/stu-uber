7e6e9d810521104818be0578ee0b1dd4b316e5e2
Excellence <oyeniranexcellenced@gmail.com>
2021-11-30 13:49:53 +0000

    setup react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="stu-drive">
+      <div className="container-fluid">
+        <div className="col-1"></div>
+        <div className="col-10">
+          <div class="landing_page_container_navigation">
+            <div class="ShopTopUp_logo">
+              <div>
+                <img src="assets/svgs/Logo.svg" alt="ShopTopUp logo" />
+              </div>
+              <hr />
+              <div>
+                <img
+                  src="assets/svgs/ShopTopUpDrive.svg"
+                  alt="ShopTopUpDrive"
+                />
+              </div>
+            </div>
+            <div class="landing_page_mini_CTA">
+              <a>
+                <span> Get started now </span>
+                <span>
+                  {/* <img src="assets/svgs/Navigate-forward.svg" /> */}
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6"></div>
+            <div className="col-6"></div>
+          </div>
+        </div>
+        <div className="col-1"></div>
+      </div>
     </div>
   );
 }
